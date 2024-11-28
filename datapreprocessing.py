a9c43669c6cad61d0f2e5b91d868f2ddc0fbc2ab
harry-tian <tianh@uchicago.edu>
2024-11-28 17:37:01 -0500

    .

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -45,7 +45,7 @@ class Loader():
         self.segment_len = segment_len
         self.batch_size = batch_size
 
-        self.alpha = Counter(list(self.Y.values()))
+        print(self.alpha)
         for i in label_dict.values():
             if i not in self.alpha:
                 self.alpha[i] = 0
