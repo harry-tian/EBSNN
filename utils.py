37e95dd11c5ed8fde085bbfee270f69b8a929aac
harry-tian <tianh@uchicago.edu>
2024-11-30 02:01:28 -0500

    started flow

--- utils.py
+++ utils.py
@@ -34,3 +34,46 @@ def deal_results(y_true, y_pred, digits=4):
     return classification_report(y_true, y_pred,
                                  output_dict=True,
                                  digits=digits)
+
+
+def bytes2bits(x):
+    return ''.join(f'{byte:08b}' for byte in x)
+def bits2bytes(x):
+    return bytes(int(x[i:i+8], 2) for i in range(0, len(x), 8))
+def mask(bits, start, end):
+    return bits[:start] + '0'*(end-start) + bits[end:]
+def bits2ints(b):
+    b = b.zfill((len(b) + 7) // 8 * 8)
+    return [int(b[i:i+8], 2) for i in range(0, len(b), 8)]
+
+def mask_ip_header(packet):
+    from scapy.all import IP
+    if IP not in packet:
+        return ''
+    
+    ip_header = bytes(packet[IP])
+    ip_header_bits = bytes2bits(ip_header)
+    U = int(ip_header_bits[4:8],2)
+    ip_header_bits = ip_header_bits[:(U*32)]
+
+    ip_header_bits = mask(ip_header_bits, 32, 48) # identification
+    ip_header_bits = mask(ip_header_bits, 80, 96) # checksum
+    ip_header_bits = mask(ip_header_bits, 96, 128) # src ip
+    ip_header_bits = mask(ip_header_bits, 128, 160) # dst ip
+    return bits2bytes(ip_header_bits)
+
+def mask_tcpudp_header(packet):
+    from scapy.all import TCP, UDP
+    if TCP in packet:
+        tcp_len = packet[TCP].dataofs
+        header = bytes(packet[TCP])[:(tcp_len*4)]
+        header_bits = bytes2bits(header)
+    elif UDP in packet:
+        header = bytes(packet[UDP])[:8]
+        header_bits = bytes2bits(header)
+    else:
+        return ''
+
+    header_bits = mask(header_bits, 0, 16) # src port
+    header_bits = mask(header_bits, 16, 32) # dst port
+    return bits2bytes(header_bits)
